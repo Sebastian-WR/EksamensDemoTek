e8e2f564ce8e89e186b84b956db294907c8f67b8
Mullefyren <57482909+Mullefyren@users.noreply.github.com>
2021-01-23 11:48:11 +0100

    Initial commit

---
+++ src/main/java/com/example/eksamensdemotek/EksamensdemotekApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamensdemotek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensdemotekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamensdemotekApplication.class, args);
+    }
+
+}
